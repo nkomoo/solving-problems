3ca39c61da00e2df7f9f02559396f59786534c7c
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-07-12 21:40:33 +0100

    Second project returning two numbers
    Multiplying the two variables

---
+++ edabit/power-calc.js
@@ -0,0 +1,9 @@
+/*
+Create a function that takes voltage and current and returns the calculated power.
+circuitPower(230, 10) ➞ 2300
+
+*/ 
+
+function circuitPower(voltage, current) {
+	return voltage*current;
+}
