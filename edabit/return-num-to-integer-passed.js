82bad984607ccdf402404c7bf0fc275080a2493d
Chantelé <105524381+nkomoo@users.noreply.github.com>
2022-07-19 01:16:49 +0100

    Create return-num-to-integer-passed.js

---
+++ edabit/return-num-to-integer-passed.js
@@ -0,0 +1,19 @@
+/* 
+
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+Examples:
+addition(0) ➞ 1
+addition(9) ➞ 10
+addition(-3) ➞ -2
+
+Notes:
+Don't forget to return the result.
+
+*/
+
+function addition(num) {
+	return ++num;
+}
+
+console.log(addition(0));
